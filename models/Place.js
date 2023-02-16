5f528180cb31c7b59410ad6cb5479d0cc142dc3c
Valto Silva <valtosilva7@gmail.com>
2023-02-16 11:44:36 -0300

    04 Created Place Model

---
+++ models/Place.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const PlaceSchema = mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  title: {
+    type: String,
+    required: [true, "Please, add a title!"],
+  },
+  address: {
+    type: String,
+    required: [true, "Please, add an address!"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please, add a description!"],
+    trim: true,
+  },
+  perks: [String],
+  extraInfo: String,
+  checkIn: Number,
+  CheckOut: Number,
+  maxGuests: Number,
+  photos: [String],
+});
+
+const PlaceModel = mongoose.model("Place", PlaceSchema);
+
+module.exports = PlaceModel;
