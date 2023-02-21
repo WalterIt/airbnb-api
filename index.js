2dfd69aa25e1d0ff84dc3a29412ec24b47c6884b
Valto Silva <valtosilva7@gmail.com>
2023-02-21 12:20:44 -0300

    11 Set Cors origin

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", 'https://vs-airbnb.vercel.app'],
   })
 );
 
