48ed0aee943066969299fbb23d36bd8cdb484442
Valto Silva <valtosilva7@gmail.com>
2023-03-02 09:27:36 -0300

    Created Upload Photos to S3 AWS. Prepared to upload to Vercel

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bcrypt = require("bcryptjs");
 const Place = require("./models/Place.js");
 const imageDownloader = require("image-downloader");
 const multer = require("multer");
+const mime = require("mime-types");
 const fs = require("fs");
 const {
   registerUser,
@@ -26,8 +27,11 @@ const {
   createBooking,
   getBooking,
 } = require("./controllers/bookingController");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = "afalj3ljfal4";
+const bucket = "vs-booking-app";
 
 app.use(express.json());
 app.use(cookieParser());
@@ -41,53 +45,86 @@ app.use(
 
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URL);
+
+async function uploadToS3(path, originalFilename, mimeType) {
+  const client = new S3Client({
+    region: "us-west-2",
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+
+  const parts = originalFilename.split(".");
+  const ext = parts[parts.length - 1];
+  const newFileName = Date.now() + "." + ext;
+
+  await client.send(
+    new PutObjectCommand({
+      Bucket: bucket,
+      Body: fs.readFileSync(path),
+      Key: newFileName,
+      ContentType: mimeType,
+      ACL: "public-read",
+    })
+  );
+
+  return `https://${bucket}.s3.amazonaws.com/${newFileName}`;
+}
 
 // USER ROUTES
-app.get("/test", async (req, res) => {
+app.get("/api/test", async (req, res) => {
   res.json("TEST PAGE OK!");
 });
 
-app.post("/register", registerUser);
-app.post("/login", loginUser);
-app.get("/profile", userProfile);
-app.post("/logout", userLogout);
+app.post("/api/register", registerUser);
+app.post("/api/login", loginUser);
+app.get("/api/profile", userProfile);
+app.post("/api/logout", userLogout);
 // console.log({ __dirname });
 
 // PLACES ROUTES
 
-app.post("/upload-by-link", async (req, res) => {
+app.post("/api/upload-by-link", async (req, res) => {
   const { link } = req.body;
   const newName = "photo" + Date.now() + ".jpg";
   await imageDownloader.image({
     url: link,
-    dest: __dirname + "/uploads/" + newName,
+    dest: "/tmp/" + newName,
   });
-  res.json(newName);
+
+  const photoUrl = await uploadToS3(
+    "/tmp/" + newName,
+    newName,
+    mime.lookup("/tmp/" + newName)
+  );
+  res.json(photoUrl);
 });
 
-const photosMiddleware = multer({ dest: "uploads/" });
-app.post("/upload", photosMiddleware.array("photos", 100), (req, res) => {
-  const uploadedFiles = [];
-  for (let i = 0; i < req.files.length; i++) {
-    const { path, originalname } = req.files[i];
-    const parts = originalname.split(".");
-    const ext = parts[parts.length - 1];
-    const newPath = path + "." + ext;
-    fs.renameSync(path, newPath);
-    uploadedFiles.push(newPath.replace("uploads\\", ""));
+const photosMiddleware = multer({ dest: "/tmp" });
+app.post(
+  "/api/upload",
+  photosMiddleware.array("photos", 100),
+  async (req, res) => {
+    const uploadedFiles = [];
+    for (let i = 0; i < req.files.length; i++) {
+      const { path, originalname, mimeType } = req.files[i];
+      const photoUrl = await uploadToS3(path, originalname, mimeType);
+      uploadedFiles.push(photoUrl);
+    }
+    res.json(uploadedFiles);
   }
-  res.json(uploadedFiles);
-});
+);
 
-app.post("/places", createPlace);
-app.get("/user-places", userPlaces);
-app.get("/places/:id", GetPlace);
-app.put("/places", updatePlace);
-app.get("/places", getPlaces);
+app.post("/api/places", createPlace);
+app.get("/api/user-places", userPlaces);
+app.get("/api/places/:id", GetPlace);
+app.put("/api/places", updatePlace);
+app.get("/api/places", getPlaces);
 
 // BOOKINGS ROUTES
-app.post("/bookings", createBooking);
-app.get("/bookings", getBooking);
+app.post("/api/bookings", createBooking);
+app.get("/api/bookings", getBooking);
 
 app.listen(4000);
